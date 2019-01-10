f7eb0af53773139ee4e4a47a4599c2bdabe213f9
Gastonm1 <gaston.marques@gmail.com>
2019-01-10 13:06:05 -0600

    Necessities

---
+++ assets/javascript/GifTastic.js
@@ -0,0 +1,7 @@
+// =====================What I will Need ==================
+//  URL = api.giphy.com =
+// Key = LlRQg08g43znkyLMjdumZmLCJQvFpkQ5
+// Path = GET /v1/gifs/search
+
+// parameters = * q: string * will allow user to search query term or phrase.
+// * limit: interger * will return a number of records (gifs) (default is 25)
